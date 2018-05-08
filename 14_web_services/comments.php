1e66505fc3e64d89e0d50e56c5406c5d6cbfbe32
bumbumbee <simona.jag@gmail.com>
2018-05-08 16:20:25 +0300

    14 tema nd

---
+++ 14_web_services/comments.php
@@ -0,0 +1,74 @@
+<?php
+
+// phpMyAdmin lenteles comments kodas:
+//CREATE TABLE comments (
+//    id INT AUTO_INCREMENT PRIMARY KEY,
+//author VARCHAR(30) NOT NULL,
+//comment TEXT,
+//created_at TIMESTAMP
+//)
+
+require ("../../../vendor/autoload.php");
+
+
+
+function getDb(){
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $db = "baltic";
+    $dsn = "mysql:host=$host;dbname=$db";
+    return new PDO($dsn, $user, $password);
+}
+
+$author="";
+$comment="";
+$created_at="";
+
+
+function createFakers(){
+    $faker = Faker\Factory::create();
+
+    $sql = "
+INSERT INTO comments (author, comment, created_at) VALUES (:author, :comment, :created_at)
+";
+
+    $sth = getDB()->prepare($sql);
+    $sth->bindParam(':author', $author);
+    $sth->bindParam(':comment', $comment);
+    $sth->bindParam(':created_at', $created_at);
+
+    // generuoja komentarus/autorius ir spausdina kiekviena sugeneruota eilute:
+    for ($i=0; $i < 10; $i++) {
+        $author=$faker->name;
+        $comment=$faker->realText($maxNbChars = 200, $indexSize = 2);
+        $created_at=$faker->date($format = 'Y-m-d', $max = 'now');
+        $sth->execute();
+        echo $author." ".$comment." ".$created_at."<br>";
+    }
+}
+
+createFakers();
+
+
+function comments_web(){
+    $sql = "SELECT comment from comments";
+
+    $data = getDB()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+    $data=array_slice($data, -10, 10, true);
+
+    $result=array();
+
+    foreach ($data as $comment){
+        array_push($result, $comment);
+    }
+    // naujausi 10 komentatu json formatu:
+    echo "<br><br>".json_encode($result)."<br>";
+}
+comments_web();
+
+
+
+
+
